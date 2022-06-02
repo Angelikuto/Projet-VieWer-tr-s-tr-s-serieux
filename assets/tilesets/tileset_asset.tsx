3364c73a49d8d005e29c0a50ab7f5e998117fa5e
thibaultdailliez <92451895+Angelikuto@users.noreply.github.com>
2022-06-02 13:06:38 +0200

    oui
    oui

---
+++ assets/tilesets/tileset_asset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tileset_asset" tilewidth="64" tileheight="64" tilecount="2176" columns="64">
+ <image source="tileset_asset.png" width="4107" height="2213"/>
+</tileset>
